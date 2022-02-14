f5d38889e5c15708565d32f3f91fb8395f268b74
Allan Kardec <54772636+kardecallan566@users.noreply.github.com>
2022-02-14 02:48:30 +0000

    Add files via upload

---
+++ Terceiro_scipt.R
@@ -0,0 +1,25 @@
+#tipo de dados
+
+
+#numerico
+
+a <- 12
+b <- 45945
+c <- 45.5
+
+d <- c / a
+d <- as.integer(c / a)
+
+#boolean
+b <- FALSE
+c <- TRUE
+
+
+
+#string
+
+a <- "alek"
+b <- "123123"
+
+#
+
